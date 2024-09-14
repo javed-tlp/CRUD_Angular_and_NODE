8c1d45c5eb23f3bfe0737f8542b8fc8af185ef1f
javed-tlp <saifijaved616@gmail.com>
2024-09-14 12:57:43 +0530

    "Updated CSS and HTML files for add-details, get-details, and home components, including styling changes and new search functionality."

--- client/src/app/get-details/get-details.component.ts
+++ client/src/app/get-details/get-details.component.ts
@@ -1,54 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from '../services/user-service.service';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
-// import { AddDetailsComponent } from '../add-details/add-details.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-get-details',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './get-details.component.html',
-  styleUrl: './get-details.component.css'
+  styleUrls: ['./get-details.component.css']
 })
 export class GetDetailsComponent implements OnInit {
 
-  constructor(private userdetails: UserServiceService) { }
-  studentData: any = []
-  data: any = []
+  studentData: any = [];
+  data: any = [];
+  filteredData: any = [];
+  searchQueryId: string = '';
+  searchQueryName: string = '';
+  searchQueryEmail: string = '';
 
+  constructor(private userdetails: UserServiceService) { }
 
   ngOnInit(): void {
     this.userdetails.getalldetails().subscribe((fetchedData) => {
       this.studentData = fetchedData;
-      console.log("studentdata", this.studentData)
-      console.log("fe", fetchedData)
-      this.assignData()
-    })
-
+      this.assignData();
+    });
   }
+
   assignData() {
-    this.data = this.studentData.data
-    console.log("data",this.data)
+    this.data = this.studentData.data;
+    this.filteredData = this.data; // Initialize filteredData with all data
+  }
+
+  onSearch() {
+    this.filteredData = this.data.filter((student: any) =>
+      (student.id.toString().includes(this.searchQueryId) || this.searchQueryId === '') &&
+      (student.Name.toLowerCase().includes(this.searchQueryName.toLowerCase()) || this.searchQueryName === '') &&
+      (student.Email.toLowerCase().includes(this.searchQueryEmail.toLowerCase()) || this.searchQueryEmail === '')
+    );
   }
 
   ondelete(stu_id: any) {
     this.userdetails.ondelete(stu_id).subscribe((result) => {
-      console.log("result:-", result)
-      alert("User deleted successfully")
-      this.ngOnInit()
-    })
-
-    // console.log(stu_id)
+      alert("User deleted successfully");
+      this.ngOnInit();
+    });
   }
+
   getuserbyID(stu_id: any) {
     this.userdetails.getuserbyID(stu_id).subscribe((result) => {
-      // console.log("User-Data:-", result)
-      
-      
-    })
+      // Handle user data here
+    });
   }
-
-
 }
