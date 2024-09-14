02501999328e1d749771abf53b12af4635157a79
javed-tlp <saifijaved616@gmail.com>
2024-09-14 23:35:42 +0530

    "Updated home component HTML and TS files, added theme toggle feature, and modified global styles in styles.css"

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,19 +1,27 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AddDetailsComponent } from '../add-details/add-details.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common'; // Import CommonModule here
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, FormsModule, AddDetailsComponent, RouterLink, NavbarComponent],
+  imports: [RouterOutlet, HttpClientModule, FormsModule, AddDetailsComponent, RouterLink, NavbarComponent, CommonModule], // Add CommonModule here
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  isDarkMode: boolean = false;
 
-
-
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
 }
